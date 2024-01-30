04a36ed94e8cd0247affe538909dc06de7a67d9c
5M-Development <156604183+5M-Development@users.noreply.github.com>
2024-01-30 20:21:01 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+## Projektname:
+- SupportMusikPIng
+
+## Installation: 
+- config.json anpassen
+- npm init
+- npm i discord.js
+- npm install opusscript
+- npm install node-opus
+- npm install @discordjs/opus
+- @discordjs/voice: 0.16.1
+
+## Verwendung:
+- Der Bot sendet eine Begrüßungsnachricht in einen konfigurierten Textkanal, wenn ein Benutzer einem bestimmten Sprachkanal beitritt. zugleich spielt der Bot 24/7 Musik.
+
+## Ersteller:
+- Code BY blckhaue
+- Projekt BY 5M-Development
+
+## Version:
+- "version": "16.13.0"
+- "opusscript": "0.0.8"
+- "discord.js": "14.14.1"
+- "@discordjs/voice": "0.16.1"
+- "libsodium-wrappers": "0.7.13"
+
+## Links:
+- Node.js: https://nodejs.org/en
+- Discord: https://discord.gg/5TqCAm7VSK
+- Developer: https://discord.com/developers/applications
+- GitHub:  https://github.com/5M-Development?tab=repositories
+- youtube: https://www.youtube.com/channel/UCAz2D4MRFhkAVd5hunBmmRw
+--------------------------------------------------------------------
---
+++ index.js
@@ -0,0 +1,215 @@
+const _0x349165 = _0x22b4;
+(function (_0x2d66d5, _0xbcbc9e) {
+    const _0x41eef4 = _0x22b4, _0x58fa48 = _0x2d66d5();
+    while (!![]) {
+        try {
+            const _0x33b51a = -parseInt(_0x41eef4(0xbc)) / (-0x556 * -0x1 + 0x7 * 0x1ac + -0x1109) + parseInt(_0x41eef4(0xf4)) / (0x1b * 0x3 + 0x45a + -0x4a9) + parseInt(_0x41eef4(0xbf)) / (-0x96 * 0x22 + 0x637 * -0x3 + 0x2694) + -parseInt(_0x41eef4(0xe4)) / (-0x67f + 0x1 * -0xde2 + -0x1 * -0x1465) + -parseInt(_0x41eef4(0xfe)) / (0x7f6 * 0x4 + -0x1f87 + -0x4c) + -parseInt(_0x41eef4(0xef)) / (0x1 * -0xecd + -0x2084 * 0x1 + 0x2f57) * (-parseInt(_0x41eef4(0xe5)) / (0xc26 * 0x1 + 0x544 * -0x1 + -0x6db)) + -parseInt(_0x41eef4(0xfc)) / (-0x1175 + -0x2621 + 0x379e) * (-parseInt(_0x41eef4(0xc4)) / (-0x1e97 * 0x1 + -0x125e + 0x30fe));
+            if (_0x33b51a === _0xbcbc9e)
+                break;
+            else
+                _0x58fa48['push'](_0x58fa48['shift']());
+        } catch (_0x13bb2f) {
+            _0x58fa48['push'](_0x58fa48['shift']());
+        }
+    }
+}(_0x5a0d, -0x18e1 * -0x45 + -0x4a000 + -0x1e4d * -0x53));
+const {Client, GatewayIntentBits} = require(_0x349165(0x101)), {joinVoiceChannel, createAudioPlayer, createAudioResource} = require(_0x349165(0xed) + _0x349165(0xfa)), {generateDependencyReport} = require(_0x349165(0xed) + _0x349165(0xfa)), config = require(_0x349165(0x105) + _0x349165(0xb8));
+console[_0x349165(0xd7)](generateDependencyReport());
+function _0x5a0d() {
+    const _0x2a3cae = [
+        'token',
+        'voiceAdapt',
+        'ajydN',
+        'stop',
+        'log',
+        'botVoiceCh',
+        'subscribe',
+        'momza',
+        'msg',
+        'cftZz',
+        'get',
+        'nnel',
+        'channels',
+        'botTextCha',
+        'login',
+        'gdUbu',
+        'XkUKA',
+        '1372712CUmtif',
+        '42bTbVZs',
+        'find',
+        'XrzTM',
+        'VC_channel',
+        'alWWW',
+        'kqdVP',
+        'idle',
+        'channelId',
+        '@discordjs',
+        'volume',
+        '1097994FzKwAy',
+        'ready',
+        'destroy',
+        'type',
+        'setSuppres',
+        '1639596lTiHUd',
+        'sed',
+        'play',
+        'annel',
+        'Guilds',
+        'channel',
+        '/voice',
+        'vGfPE',
+        '3053384hAvHds',
+        'teamid',
+        '5996155RrejTd',
+        'Update',
+        'GUILD_STAG',
+        'discord.js',
+        'once',
+        'Die\x20Rolle\x20',
+        'GuildVoice',
+        './config.j',
+        'mit\x20dem\x20Na',
+        'E_VOICE',
+        'musik_mp3',
+        'setVolume',
+        'send',
+        'cht\x20gefund',
+        'error',
+        'son',
+        'voiceState',
+        'm\x20Senden\x20d',
+        'men\x20\x27',
+        '363800vEXGOX',
+        'GhvLA',
+        'voice',
+        '1160796AWucHP',
+        '\x27\x20wurde\x20ni',
+        'en.',
+        'guild',
+        'States',
+        '9NkxzOa',
+        'Fehler\x20bei',
+        'cache',
+        'lcHRm',
+        'Qsodh',
+        'er\x20Nachric',
+        'catch',
+        'suppress',
+        'TYbyT',
+        'ht:',
+        'zzUbC',
+        'roles',
+        'erCreator',
+        'then',
+        'fetch'
+    ];
+    _0x5a0d = function () {
+        return _0x2a3cae;
+    };
+    return _0x5a0d();
+}
+const client = new Client({
+    'intents': [
+        GatewayIntentBits[_0x349165(0xf8)],
+        GatewayIntentBits[_0x349165(0x104) + _0x349165(0xc3)]
+    ]
+});
+function _0x22b4(_0x552477, _0x19975c) {
+    const _0x576587 = _0x5a0d();
+    return _0x22b4 = function (_0x212dc7, _0x539bf9) {
+        _0x212dc7 = _0x212dc7 - (-0x7 * 0x423 + 0x1cfe + 0xaf);
+        let _0x4e581c = _0x576587[_0x212dc7];
+        return _0x4e581c;
+    }, _0x22b4(_0x552477, _0x19975c);
+}
+client[_0x349165(0xe1)](config[_0x349165(0xd3)]);
+const Channels = config[_0x349165(0xe8) + 's'];
+client[_0x349165(0x102)](_0x349165(0xf0), async () => {
+    const _0x42088b = _0x349165, _0x4b08bc = {
+            'kqdVP': function (_0xf6ba42, _0x1263d5) {
+                return _0xf6ba42(_0x1263d5);
+            },
+            'zzUbC': function (_0x2010d9, _0x2417b2, _0x44ca82) {
+                return _0x2010d9(_0x2417b2, _0x44ca82);
+            },
+            'XrzTM': function (_0x462ec9) {
+                return _0x462ec9();
+            },
+            'XkUKA': _0x42088b(0xeb),
+            'momza': function (_0x3d4e44, _0x328164) {
+                return _0x3d4e44(_0x328164);
+            }
+        };
+    for (const _0x9e660c of Channels) {
+        _0x4b08bc[_0x42088b(0xda)](_0x53a1bb, _0x9e660c), await new Promise(_0x1b9b6b => setTimeout(_0x1b9b6b, 0xc7 * 0x8 + -0x371 * -0xb + 0x2a1f * -0x1));
+    }
+    function _0x53a1bb(_0x30f3a3) {
+        const _0x4f3cfd = _0x42088b, _0x4c68a7 = {
+                'cftZz': function (_0x4d8fea, _0x44e566) {
+                    const _0x572e2e = _0x22b4;
+                    return _0x4b08bc[_0x572e2e(0xea)](_0x4d8fea, _0x44e566);
+                },
+                'ajydN': function (_0x242ef9, _0x124a14) {
+                    const _0x29e92d = _0x22b4;
+                    return _0x4b08bc[_0x29e92d(0xea)](_0x242ef9, _0x124a14);
+                },
+                'lcHRm': function (_0x47daa3, _0x2890f6, _0x47fb07) {
+                    const _0x4b086d = _0x22b4;
+                    return _0x4b08bc[_0x4b086d(0xce)](_0x47daa3, _0x2890f6, _0x47fb07);
+                },
+                'alWWW': function (_0x14c8f9) {
+                    const _0x172537 = _0x22b4;
+                    return _0x4b08bc[_0x172537(0xe7)](_0x14c8f9);
+                },
+                'vGfPE': _0x4b08bc[_0x4f3cfd(0xe3)]
+            };
+        client[_0x4f3cfd(0xdf)][_0x4f3cfd(0xd2)](_0x30f3a3)[_0x4f3cfd(0xd1)](_0x218600 => {
+            const _0x3a150f = _0x4f3cfd, _0x3e72b3 = _0x4c68a7[_0x3a150f(0xd5)](joinVoiceChannel, {
+                    'channelId': _0x218600['id'],
+                    'guildId': _0x218600[_0x3a150f(0xc2)]['id'],
+                    'adapterCreator': _0x218600[_0x3a150f(0xc2)][_0x3a150f(0xd4) + _0x3a150f(0xd0)]
+                }), _0x34d9fd = _0x4c68a7[_0x3a150f(0xc7)](createAudioResource, config[_0x3a150f(0x108)], { 'inlineVolume': !![] });
+            _0x34d9fd[_0x3a150f(0xee)][_0x3a150f(0x109)](0x502 * -0x3 + -0x1777 + 0x267d + 0.2);
+            const _0x5f1d77 = _0x4c68a7[_0x3a150f(0xe9)](createAudioPlayer);
+            _0x3e72b3[_0x3a150f(0xd9)](_0x5f1d77), _0x5f1d77[_0x3a150f(0xf6)](_0x34d9fd), _0x5f1d77['on'](_0x4c68a7[_0x3a150f(0xfb)], () => {
+                const _0x2a8eb7 = _0x3a150f;
+                try {
+                    _0x5f1d77[_0x2a8eb7(0xd6)]();
+                } catch (_0x43f516) {
+                }
+                try {
+                    _0x3e72b3[_0x2a8eb7(0xf1)]();
+                } catch (_0x3df6c7) {
+                }
+                _0x4c68a7[_0x2a8eb7(0xdc)](_0x53a1bb, _0x218600['id']);
+            });
+        })[_0x4f3cfd(0xca)](console[_0x4f3cfd(0x10c)]);
+    }
+}), client['on'](_0x349165(0xb9) + _0x349165(0xff), async (_0x2b8a4c, _0x1c00b1) => {
+    const _0x235b4e = _0x349165, _0x234b34 = {
+            'TYbyT': function (_0x9248b2, _0x14b570) {
+                return _0x9248b2 === _0x14b570;
+            },
+            'gdUbu': _0x235b4e(0xc5) + _0x235b4e(0xba) + _0x235b4e(0xc9) + _0x235b4e(0xcd),
+            'GhvLA': function (_0x16ed51, _0x593a39) {
+                return _0x16ed51 === _0x593a39;
+            },
+            'Qsodh': _0x235b4e(0x100) + _0x235b4e(0x107)
+        };
+    if (_0x1c00b1[_0x235b4e(0xec)] && _0x234b34[_0x235b4e(0xcc)](_0x1c00b1[_0x235b4e(0xf9)]['id'], config[_0x235b4e(0xd8) + _0x235b4e(0xf7)])) {
+        const _0x3f4b26 = client[_0x235b4e(0xdf)][_0x235b4e(0xc6)][_0x235b4e(0xdd)](config[_0x235b4e(0xe0) + _0x235b4e(0xde)]);
+        if (_0x3f4b26)
+            try {
+                const _0x2b094e = config[_0x235b4e(0xfd)], _0x419e82 = _0x1c00b1[_0x235b4e(0xc2)][_0x235b4e(0xcf)][_0x235b4e(0xc6)][_0x235b4e(0xe6)](_0xa5b0c8 => _0xa5b0c8['id'] === _0x2b094e);
+                _0x419e82 ? await _0x3f4b26[_0x235b4e(0x10a)](config[_0x235b4e(0xdb)]) : (console[_0x235b4e(0x10c)](_0x235b4e(0x103) + _0x235b4e(0x106) + _0x235b4e(0xbb) + _0x2b094e + (_0x235b4e(0xc0) + _0x235b4e(0x10b) + _0x235b4e(0xc1))), await _0x3f4b26[_0x235b4e(0x10a)](config[_0x235b4e(0xdb)]));
+            } catch (_0x45ea5c) {
+                console[_0x235b4e(0x10c)](_0x234b34[_0x235b4e(0xe2)], _0x45ea5c);
+            }
+    }
+    if (_0x1c00b1[_0x235b4e(0xec)] && _0x234b34[_0x235b4e(0xbd)](_0x1c00b1[_0x235b4e(0xf9)][_0x235b4e(0xf2)], _0x234b34[_0x235b4e(0xc8)]) && _0x1c00b1[_0x235b4e(0xc2)]['me'][_0x235b4e(0xbe)][_0x235b4e(0xcb)])
+        try {
+            await _0x1c00b1[_0x235b4e(0xc2)]['me'][_0x235b4e(0xbe)][_0x235b4e(0xf3) + _0x235b4e(0xf5)](![]);
+        } catch (_0xd93257) {
+            console[_0x235b4e(0x10c)](_0xd93257);
+        }
+});
